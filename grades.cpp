f8a70a6c1d42975b370b9d8b27d7e91c72e5d537
Rayona J Pryce <30443160+Prycrj@users.noreply.github.com>
2018-03-23 22:50:14 -0400

    Problem 1

---
+++ grades.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#define SIZE 10
+
+#include "grades.cc"
+
+using namespace std;
+
+int main()
+{
+ GPA info;
+
+ cout<<info.getGPA();
+
+ return 0;
+}
---
+++ grades.h
@@ -0,0 +1,12 @@
+#ifndef GRADES_H
+#define GRADES_H
+
+class GPA{
+  private:
+  int grades[SIZE]={4,3,3,2,4,3,2,4,4,0};
+
+  public:
+
+  double getGPA();
+};
+#endif
